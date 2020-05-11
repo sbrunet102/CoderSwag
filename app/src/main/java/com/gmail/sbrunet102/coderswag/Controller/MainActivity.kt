3cb5cab6a0d04e480fb98ab1bd7662da3fba3efa
Sylvain Brunet <sbrunet102@gmail.com>
2020-05-11 15:08:48 -0400

    lesson 02

--- app/src/main/java/com/gmail/sbrunet102/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/gmail/sbrunet102/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,23 @@ package com.gmail.sbrunet102.coderswag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.gmail.sbrunet102.coderswag.Model.Category
 import com.gmail.sbrunet102.coderswag.R
+import com.gmail.sbrunet102.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/com/gmail/sbrunet102/coderswag/Model/Category.kt
+++ app/src/main/java/com/gmail/sbrunet102/coderswag/Model/Category.kt
@@ -1,3 +1,7 @@
 package com.gmail.sbrunet102.coderswag.Model
 
-class Category(val title:String,val image:String)
+class Category(val title:String,val image:String){
+    override fun toString(): String {
+        return title
+    }
+}
